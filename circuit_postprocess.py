64df9cd172b90921d4abc7499669e6f56bc57703
wilson <18582007+0mWh@users.noreply.github.com>
2025-07-01 19:04:56 -0400

    accelerate calculation of overlap

--- circuit_postprocess.py
+++ circuit_postprocess.py
@@ -1,37 +1,46 @@
-def swap_expectation(counts:dict[str,int]|list[int], size:int=9) -> tuple[float,float]:
-	"""
-	Turn raw measurement `counts` into	⟨SWAP⟩	and overlap |⟨φ(y)|φ(x)⟩|².
-	counts:
-		counts[bitstring] = frequency
-		counts = [all frequencies...]
-	= ⟨SWAP⟩, overlap
-	"""
+from os import environ as ENV
 
-	if isinstance(counts, dict):
-		total = sum(counts.values())
-		f = sum(
-			(-1) ** sum(
-				int(si) * int(ti)
-				for ti, si in zip(bitstring[:size], bitstring[size:])
-			) * freq / total
-			for bitstring, freq in counts.items()
-		)
+import numpy as np
+import numpy.typing as npt
+Float = np.float64
+
+import diskcache as dc
+cache = dc.Cache(
+    ENV['HOME'] + '/.cache/postprocess/', # this longevity does not matter
+    size_limit = 2 ** 32, # 4GB
+)
+
+@cache.memoize()
+def swap_expectation_table(size:int=9) -> npt.NDArray[Float]:
+	# (-1) ** sum(s_i * t_i)
+	return (-1) ** sum(
+		((np.indices((2**(size+size),)) & (1<<si)) >> si)
+		*
+		((np.indices((2**(size+size),)) & (1<<ti)) >> ti)
+		for si, ti in zip(range(size), range(size+1,size+size))
+	)
+
+def swap_expectation(ans, size:int=9) -> tuple[Float,Float]:
+	''' Convert measurement into ⟨SWAP⟩ and overlap |⟨φ(y)|φ(x)⟩|². '''
+
+	def swap_expectation_list(probs:npt.NDArray[Float]) -> tuple[Float,Float]:
+		''' counts = [probabilities...] '''
+		t = swap_expectation_table(size)
+		f = np.sum(t * probs)
+		return f, (1+f)/2
 	
+	def swap_expectation_dict(counts:dict[str,Float]) -> tuple[Float,Float]:
+		''' counts[bitstring] = frequency '''
+		probs = np.zeros((2**(size+size),))
+		probs[[tuple(counts.keys())]] = list(counts.values())
+		return swap_expectation_list(probs, size)
+
+	if isinstance(ans, dict):
+		return swap_expectation_dict(ans)
 	else:
-		total = sum(counts)
-		f = sum(
-			(-1) ** sum(
-				((i & 1<<si)>>si) * ((i & 1<<ti)>>ti)
-				for si, ti in zip(range(size), range(size+1,size+size))
-			) * freq / total
-			for i, freq in enumerate(counts)
-		)
-	
-	fidelity = (1+f)/2
-	
-	return f, fidelity
+		return swap_expectation_list(ans)
 
-def swap_expectation_og(counts, n_pairs=9):
+def swap_expectation_og(counts:dict[str,int], n_pairs:int=9) -> tuple[float,float]:
     """
     Turn raw measurement `counts` into  ⟨SWAP⟩  and overlap |⟨φ(y)|φ(x)⟩|².
     counts  : dict  bitstring -> frequency   (keys length = 2*n_pairs)
