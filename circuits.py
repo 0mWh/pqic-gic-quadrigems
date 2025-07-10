1049cf2d5d8f0221291d71920b4a26bd7d3394bc
wilson <18582007+0mWh@users.noreply.github.com>
2025-07-10 18:27:38 -0400

    Wednesday and Thursday

--- circuits.py
+++ circuits.py
@@ -1,7 +1,11 @@
 import pennylane as qml
+import numpy as np
 
 def circuit_with_dev(circuit, dev):
-	return qml.qjit(seed=0)(qml.qnode(dev)(circuit))
+    return qml.qjit(seed=0)(qml.qnode(dev)(circuit))
+
+def executor(dev):
+    return lambda circuit: qml.qjit(seed = 0)(qml.qnode(dev)(circuit))
 
 # angle / iqp / amplitude
 # qft / iqft
@@ -15,8 +19,8 @@ def circuit_angle_qft_swap(a, b):
     # 1. Data: Angle Embedding
     qml.AngleEmbedding(a, wires=range(la))
     qml.AngleEmbedding(b, wires=range(la, la + lb))
-	
-	# 2. Nonlinear Transform: QFT
+    
+    # 2. Nonlinear Transform: QFT
     qml.QFT(wires = range(la))
     qml.QFT(wires = range(la, la + lb))
 
@@ -26,26 +30,74 @@ def circuit_angle_qft_swap(a, b):
     qml.Barrier()
     for i in range(la):
         qml.H(i)
-	
+    
+    return qml.probs(wires = range(la + lb))
+
+# Angle Embed -> iQFT -> SWAP
+def circuit_angle_iqft_swap(a, b):
+    la, lb = len(a), len(b)
+    assert la == lb
+
+    # 1. Data: Angle Embedding
+    qml.AngleEmbedding(a, wires=range(la))
+    qml.AngleEmbedding(b, wires=range(la, la + lb))
+    
+    # 2. Nonlinear Transform: QFT
+    qml.adjoint(
+    qml.QFT(wires = range(la))
+    )
+    qml.adjoint(
+    qml.QFT(wires = range(la, la + lb))
+    )
+
+    # 3. Correlation: SWAP Test
+    for i in range(la):
+        qml.CNOT(wires = [i, la + i])
+    qml.Barrier()
+    for i in range(la):
+        qml.H(i)
+    
     return qml.probs(wires = range(la + lb))
 
 # Polynomial Embed -> iQFT -> SWAP
 def circuit_iqp_iqft_swap(a, b):
     la, lb = len(a), len(b)
-	assert la == lb
+    assert la == lb
 
-	# 1. Data: Instantaneous Quantum Polynomial (IQP)
+    # 1. Data: Instantaneous Quantum Polynomial (IQP)
     qml.IQPEmbedding(a, wires=range(la), n_repeats=2)
     qml.IQPEmbedding(b, wires=range(la, la + lb), n_repeats=2)
 
-	# 2. Nonlinear Transform: inverse QFT
+    # 2. Nonlinear Transform: inverse QFT
     qml.adjoint(qml.QFT(wires = range(la)))
     qml.adjoint(qml.QFT(wires = range(la, la + lb)))
 
-	# 3. Correlation: SWAP Test
+    # 3. Correlation: SWAP Test
     for i in range(la):
         qml.CNOT(wires = [i, la + i])
-	qml.Barrier()
+    qml.Barrier()
+    for i in range(la):
+        qml.H(i)
+
+    return qml.probs(wires = range(la + lb))
+
+# Polynomial Embed -> QFT -> SWAP
+def circuit_iqp_qft_swap(a, b):
+    la, lb = len(a), len(b)
+    assert la == lb
+
+    # 1. Data: Instantaneous Quantum Polynomial (IQP)
+    qml.IQPEmbedding(a, wires=range(la), n_repeats=2)
+    qml.IQPEmbedding(b, wires=range(la, la + lb), n_repeats=2)
+
+    # 2. Nonlinear Transform: inverse QFT
+    qml.QFT(wires = range(la))
+    qml.QFT(wires = range(la, la + lb))
+
+    # 3. Correlation: SWAP Test
+    for i in range(la):
+        qml.CNOT(wires = [i, la + i])
+    qml.Barrier()
     for i in range(la):
         qml.H(i)
 
@@ -53,43 +105,124 @@ def circuit_iqp_iqft_swap(a, b):
 
 # Amplitude Encoding overlap [Figure S.5B]
 def circuit_amp_iamp(a, b):
+    """
+    Drawing
+    0: ──H─╭|Ψ⟩──H─╭|Ψ⟩─╭|Ψ⟩†──H─╭|Ψ⟩†──H─┤ ╭Probs
+    1: ──H─├|Ψ⟩──H─├|Ψ⟩─├|Ψ⟩†──H─├|Ψ⟩†──H─┤ ├Probs
+    2: ──H─├|Ψ⟩──H─├|Ψ⟩─├|Ψ⟩†──H─├|Ψ⟩†──H─┤ ├Probs
+    3: ──H─╰|Ψ⟩──H─╰|Ψ⟩─╰|Ψ⟩†──H─╰|Ψ⟩†──H─┤ ╰Probs
+    """
     la, lb = len(a), len(b)
-	assert la == lb
+    assert la == lb
+    # assert power of 2
+    n_wires = np.log2(la)
+    assert n_wires == int(n_wires)
+    n_wires = int(n_wires)
+    # print(la, lb)
 
-	# 1. Data: Amplitude Embedding
-	for i in range(2):
-		for j in range(la):
-	        qml.H(j)
-		qml.AmplitudeEmbedding(a, wires = range(la), normalize = True)
-
-	# 3. Correlation: Amplitude Un-Embedding
-	for i in range(2):
-		qml.adjoint(
-			qml.AmplitudeEmbedding(b, wires = range(lb), normalize = True)
-		)
-		for j in range(la):
-	        qml.H(j)
+    # 1. Data: Amplitude Embedding
+    for i in range(2):
+        for j in range(n_wires):
+            qml.H(j)
+        qml.AmplitudeEmbedding(a, wires = range(n_wires), normalize = True)
+        # qml.Barrier()
+        
+    # 2. Correlation: Amplitude Un-Embedding
+    for i in range(2):
+        qml.adjoint(
+            qml.AmplitudeEmbedding(b, wires = range(n_wires), normalize = True)
+        )
+        # qml.Barrier()
+        for j in range(n_wires):
+            qml.H(j)
     
-	return qml.probs(wires = range(la + lb))
+    return qml.probs(wires = range(n_wires))
 
+# Amplitude Encoding overlap [Figure S.5B]
+# the fidelity is the probability of the 0 state
+def circuit_amp_iamp_qft_canonical(a, b):
+    """
+    Drawing
+    0: ──H─╭|Ψ⟩─╭QFT──H─╭|Ψ⟩─╭QFT─╭QFT†─╭|Ψ⟩†──H─╭QFT†─╭|Ψ⟩†──H─┤ ╭Probs
+    1: ──H─├|Ψ⟩─├QFT──H─├|Ψ⟩─├QFT─├QFT†─├|Ψ⟩†──H─├QFT†─├|Ψ⟩†──H─┤ ├Probs
+    2: ──H─├|Ψ⟩─├QFT──H─├|Ψ⟩─├QFT─├QFT†─├|Ψ⟩†──H─├QFT†─├|Ψ⟩†──H─┤ ├Probs
+    3: ──H─╰|Ψ⟩─╰QFT──H─╰|Ψ⟩─╰QFT─╰QFT†─╰|Ψ⟩†──H─╰QFT†─╰|Ψ⟩†──H─┤ ╰Probs
+    """
+    la, lb = len(a), len(b)
+    assert la == lb
+    # assert power of 2
+    n_wires = np.log2(la)
+    assert n_wires == int(n_wires)
+    n_wires = int(n_wires)
+    # print(la, lb)
+
+    def state_prep(data, wires):
+        qml.AmplitudeEmbedding(data, wires = wires, normalize = True)
+        # qml.Barrier()
+        qml.QFT(wires = wires)
+        # qml.Barrier()
+        
+    la, lb = len(a), len(b)
+    assert la == lb
+
+    # 1. Data: Amplitude Embedding
+    for i in range(2):
+        for j in range(n_wires):
+            qml.H(j)
+        state_prep(a, range(n_wires))
+
+    # 2. Correlation: Amplitude Un-Embedding
+    for i in range(2):
+        qml.adjoint(state_prep)(b, range(n_wires))
+        for i in range(n_wires):
+            qml.H(i)
+    
+    return qml.probs(wires = range(n_wires))
+
+# we can write a more efficient form by eliminate the QFT-iQFT in the middle, these should cancel out
+def circuit_amp_iamp_qft(a, b):
+    la, lb = len(a), len(b)
+    assert la == lb
+    # assert power of 2
+    n_wires = np.log2(la)
+    assert n_wires == int(n_wires)
+    n_wires = int(n_wires)
+    # print(la, lb)
+
+    # 1. Data: Amplitude Embedding
+    for i in range(n_wires): qml.H(i)
+    qml.AmplitudeEmbedding(a, wires = range(n_wires), normalize = True)
+    qml.QFT(wires = range(n_wires))
+    for i in range(n_wires): qml.H(i)
+    qml.AmplitudeEmbedding(a, wires = range(n_wires), normalize = True)
+    
+    # 2. Correlation: Amplitude Un-Embedding
+    qml.adjoint(qml.AmplitudeEmbedding(b, wires = range(n_wires), normalize = True))
+    for i in range(n_wires): qml.H(i)
+    qml.adjoint(qml.QFT(wires = range(n_wires)))
+    qml.adjoint(qml.AmplitudeEmbedding(b, wires = range(n_wires), normalize = True))
+    for i in range(n_wires): qml.H(i)
+    
+    return qml.probs(wires = range(n_wires))
+    
 # Polynomial Embed -> QFT -> SWAP
 def circuit_iqpembed_qft_swap(a, b):
     la, lb = len(a), len(b)
-	assert la == lb
+    assert la == lb
 
-	# 1. Data: Instantaneous Quantum Polynomial (IQP)
+    # 1. Data: Instantaneous Quantum Polynomial (IQP)
     qml.IQPEmbedding(a, wires=range(la), n_repeats=2)
     qml.IQPEmbedding(b, wires=range(la, la + lb), n_repeats=2)
 
-	# 2. Nonlinear Transform: QFT, DFT
-	qml.QFT(wires = range(la))
-	qml.QFT(wires = range(la, la + lb))
+    # 2. Nonlinear Transform: QFT, DFT
+    qml.QFT(wires = range(la))
+    qml.QFT(wires = range(la, la + lb))
     # qml.adjoint(qml.QFT(wires = range(la, la + lb))) # iQFT = DFT
 
-	# 3. Correlation: SWAP Test
+    # 3. Correlation: SWAP Test
     for i in range(la):
         qml.CNOT(wires = [i, la + i])
-	qml.Barrier()
+    qml.Barrier()
     for i in range(la + lb):
         qml.H(i)
 
@@ -100,26 +233,30 @@ def circuit_iqpembed_qft_swap(a, b):
 # Amplitude Embed -> QFT ->
 def circuit_ampembed_something(a, b):
     la, lb = len(a), len(b)
-	assert la == lb
+    assert la == lb
 
-	for j in range(la + lb):
-		qml.H(j)
+    for j in range(la + lb):
+        qml.H(j)
 
-	# 1. Data: Amplutide
-	qml.AmplitudeEmbedding(a, wires = range(la), normalize = True)
+    # 1. Data: Amplutide
+    qml.AmplitudeEmbedding(a, wires = range(la), normalize = True)
 
     # 2. Nonlinear Transform: QFT
     qml.QFT(wires = range(la))
 
     # 3. Correlation: Unembed and uncompute, expecting other data
-	qml.adjoint(
-		qml.AmplitudeEmbedding(b, wires = range(lb), normalize = True)
-	)
-	qml.adjoint(
-		qml.QFT(wires = range(lb))
-	)
+    qml.adjoint(
+        qml.AmplitudeEmbedding(b, wires = range(lb), normalize = True)
+    )
+    qml.adjoint(
+        qml.QFT(wires = range(lb))
+    )
 
-	for j in range(la):
-		qml.H(j)
+    for j in range(la):
+        qml.H(j)
 
-	return qml.probs(wires = range(la))
+    return qml.probs(wires = range(la))
+
+"""
+The quantum Fourier transform is the classical discrete Fourier transform applied to the vector of amplitudes of a quantum state
+"""
--- neurodata.py
+++ neurodata.py
@@ -1,9 +1,27 @@
 from os import environ as ENV
 
 import scipy
+from scipy import stats
+
 import pandas as pd
-import statsmodels
 import numpy as np
+
+# monkeypatch: statsmodels with scipy>=1.16
+# scipy._lib.{_util._lazywhere -> array_api_extra.apply_where}
+#  scipy._lib._util._lazywhere:
+#  - https://pydocs.github.io/p/scipy/1.8.0/api/scipy._lib._util._lazywhere.html
+#  @ https://github.com/scipy/scipy/commit/0a315773203ce8e20cad3e800f82b2beaef26bdf#diff-d28fbb0be287769c763ce61b0695feb0a6ca0e67b28bafee20526b46be7aaa84L21-L75
+#  scipy._lib.array_api_extra.apply_where
+#  + https://data-apis.org/array-api-extra/generated/array_api_extra.apply_where.html
+#  @ https://github.com/data-apis/array-api-extra/blob/de481f2cac821c2db7ab2a45b83ed29963c2e1eb/src/array_api_extra/_lib/_funcs.py#L62
+import scipy._lib._util
+scipy._lib._util._lazywhere = \
+    lambda cond, arrays, f, fillvalue=None, f2=None: \
+    scipy._lib.array_api_extra.apply_where(cond, arrays, f, f2=f2, fill_value=fillvalue)
+
+import statsmodels.api as sm
+import statsmodels
+
 
 DEFAULT_RECORD = ENV['HOME'] + '/work/auditory_cortex_data/081920_355r/allPlanesVariables27-Feb-2021.mat'
 
@@ -42,6 +60,127 @@ def get_coords(record):
 	zs = pd.concat([pd.DataFrame(record['allzc'][plane][0]) for plane in range(1,6)]).rename(columns={0: 'z'}).reset_index().drop(columns='index')
 	coords = pd.concat([xs, ys, zs], axis=1)
 	return coords
+
+def rentian_scaling(A, xyz, n, seed=42):
+    """
+    An updated Rentian Scaling function from bctpy, which makes random sized cubes at random positions.
+    """
+    rng = np.random.default_rng(seed)
+    m = np.size(xyz, axis=0)  # find number of nodes in system
+
+    # rescale coordinates so they are all greater than unity
+    xyzn = xyz - np.tile(np.min(xyz, axis=0) - 1, (m, 1))
+
+    # find the absolute minimum and maximum over all directions
+    nmax = np.max(xyzn)
+    nmin = np.min(xyzn)
+
+    count = 0
+    N = np.zeros((n,))
+    E = np.zeros((n,))
+
+    # create partitions and count the number of nodes inside the partition (n)
+    # and the number of edges traversing the boundary of the partition (e)
+    while count < n:
+        # define cube endpoints
+        randx = np.sort((nmax - nmin) * rng.random((2,)))
+        randy = np.sort((nmax - nmin) * rng.random((2,)))
+        randz = np.sort((nmax - nmin) * rng.random((2,)))
+
+        # ensure cube is within network boundaries
+        randx[0] = max(randx[0], nmin)
+        randx[1] = min(randx[1], nmax)
+        randy[0] = max(randy[0], nmin)
+        randy[1] = min(randy[1], nmax)
+        randz[0] = max(randz[0], nmin)
+        randz[1] = min(randz[1], nmax)
+
+        # find nodes in cube
+        l1 = xyzn[:, 0] > randx[0]
+        l2 = xyzn[:, 0] < randx[1]
+        l3 = xyzn[:, 1] > randx[0]
+        l4 = xyzn[:, 1] < randx[1]
+        l5 = xyzn[:, 2] > randx[0]
+        l6 = xyzn[:, 2] < randx[1]
+
+        L, = np.where((l1 & l2 & l3 & l4 & l5 & l6).flatten())
+        if np.size(L):
+            # count edges crossing at the boundary of the cube
+            E[count] = np.sum(A[np.ix_(L, np.setdiff1d(range(m), L))])
+            # count nodes inside of the cube
+            N[count] = np.size(L)
+            count += 1
+
+    return N, E
+
+
+def rent_exp(A, coords, M=20):
+    """
+    Compute Rent's exponent with robust fit method
+    """
+    # M = 1000  # number of partitions, if we have at least M more than neurons, then each partition has about 1 neuron
+
+    N, E = rentian_scaling(A, coords.to_numpy(), M, seed=42)
+    # recommended step is to prune partitions with N < M/2
+    N_prime = N # [N < M/2]
+    E_prime = E # [N < M/2]
+
+    # Log-log transformation
+    log_N_prime = np.log10(N_prime)
+    log_E_prime = np.log10(E_prime)
+
+    # Add a constant (intercept) to the independent value
+    log_N_prime = sm.add_constant(log_N_prime)
+
+    # Fit the robust linear model
+    model = sm.RLM(log_E_prime, log_N_prime, M=statsmodels.robust.norms.TukeyBiweight()) # , M=sm.robust.norms.HuberT())
+    results = model.fit()
+
+    # The Rent's exponent is the coefficient of the slope
+    rent_exponent = results.params[1]
+    print("Rent's exponent:", rent_exponent)
+
+    # Standard error of the estimation
+    std_error = results.bse[1]
+    print("Standard error of the estimation:", std_error)
+    return rent_exponent, std_error
+
+from sklearn.metrics import r2_score
+import numpy as np
+from scipy.optimize import curve_fit
+
+def is_rentian_scaling(A, coords, M = 20):
+    
+    # Define the power law function
+    def power_law(x, a, b):
+        return a * x**b
+    
+    # Define the exponential law function
+    def exponential_law(x, a, b):
+        return a * np.exp(b * x)
+    
+    N, E = rentian_scaling(A, coords.to_numpy(), M)
+    X, Y = N, E
+    
+    # Fit the power law
+    popt_power, pcov_power = curve_fit(power_law, X, Y)
+    y_fit_power = power_law(X, *popt_power)
+    
+    # Fit the exponential law
+    popt_exponential, pcov_exponential = curve_fit(exponential_law, X, Y)
+    y_fit_exponential = exponential_law(X, *popt_exponential)
+
+    # Calculate the R^2 values
+    r2_power = r2_score(Y, y_fit_power)
+    r2_exponential = r2_score(Y, y_fit_exponential)
+    
+    print("Power law R^2: {:.2f}".format(r2_power))
+    print("Exponential law R^2: {:.2f}".format(r2_exponential))
+    if r2_power > r2_exponential:
+        print('Rentian scaling achieved')
+    else:
+        print('Rentian scaling not achieved')
+    return r2_power, r2_exponential
 
 # tuning_curves = pd.concat(
 # 	[
